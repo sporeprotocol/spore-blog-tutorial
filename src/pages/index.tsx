55b657cda32270330ad30f817f9bc30ba4927026
ahonn <ahonn95@outlook.com>
2023-09-06 16:07:38 +0800

    feat: use wagmi to connect ethereum

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,21 @@
+import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import { InjectedConnector } from 'wagmi/connectors/injected';
+
 export default function Home() {
-  return <main>Hello World</main>;
+  const { address, isConnected } = useAccount();
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  });
+  const { disconnect } = useDisconnect();
+
+  return (
+    <div>
+      <div>{address}</div>
+      {isConnected ? (
+        <button onClick={() => disconnect()}>Disconnect</button>
+      ) : (
+        <button onClick={() => connect()}>Connect Wallet</button>
+      )}
+    </div>
+  );
 }
