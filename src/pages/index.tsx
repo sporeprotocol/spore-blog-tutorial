58fcf1d5d6d7d64263d98725373776cd5d778177
ahonn <ahonn95@outlook.com>
2023-09-06 16:19:47 +0800

    feat: generate ckb address based on eth address

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,31 @@
+import { commons, config, helpers } from '@ckb-lumos/lumos';
+import { useMemo } from 'react';
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 
 export default function Home() {
-  const { address, isConnected } = useAccount();
+  const { address: ethAddress, isConnected } = useAccount();
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   });
   const { disconnect } = useDisconnect();
 
+  const address = useMemo(() => {
+    if (!ethAddress) return;
+
+    const lock = commons.omnilock.createOmnilockScript({
+      auth: { flag: 'ETHEREUM', content: ethAddress ?? '0x' },
+    });
+    return helpers.encodeToAddress(lock, { config: config.predefined.AGGRON4 });
+  }, [ethAddress]);
+
   return (
     <div>
-      <div>{address}</div>
       {isConnected ? (
-        <button onClick={() => disconnect()}>Disconnect</button>
+        <div>
+          <div>CKB Address: {address}</div>
+          <button onClick={() => disconnect()}>Disconnect</button>
+        </div>
       ) : (
         <button onClick={() => connect()}>Connect Wallet</button>
       )}
