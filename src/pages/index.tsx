ac3169b24219b934aaf4e013bc696c6273b19f30
ahonn <ahonn95@outlook.com>
2023-09-11 22:42:06 +0800

    feat: adjust page structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,108 +1,94 @@
 import useWallet from '@/hooks/useWallet';
 import { hex2String } from '@/utils/helpers';
-import { signTransaction } from '@/utils/transaction';
-import { Indexer, RPC, config, helpers } from '@ckb-lumos/lumos';
+import { Indexer } from '@ckb-lumos/lumos';
 import {
   ClusterData,
-  createCluster,
+  SporeData,
   predefinedSporeConfigs,
 } from '@spore-sdk/core';
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
-import { Site } from './type';
+import { useCallback } from 'react';
+import { Post, Site } from './type';
+import Link from 'next/link';
 
-export default function Home() {
-  const { address, lock, balance, isConnected, connect, disconnect } =
-    useWallet();
-  const [sites, setSites] = useState<Site[]>([]);
-  const [siteName, setSiteName] = useState('');
-  const [siteDescription, setSiteDescription] = useState('');
+const id = process.env.NEXT_PUBLIC_SITE_ID;
+
+export default function SitePage() {
+  const { lock } = useWallet();
+  const [siteInfo, setSiteInfo] = useState<Site>();
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const fetchSiteInfo = useCallback(async () => {
+    const indexer = new Indexer(predefinedSporeConfigs.Aggron4.ckbIndexerUrl);
+    const { script } = predefinedSporeConfigs.Aggron4.scripts.Cluster;
+    const collector = indexer.collector({
+      type: { ...script, args: id as string },
+    });
+
+    for await (const cell of collector.collect()) {
+      const unpacked = ClusterData.unpack(cell.data);
+      setSiteInfo({
+        id: cell.cellOutput.type!.args,
+        name: hex2String(unpacked.name.slice(2)),
+        description: hex2String(unpacked.description.slice(2)),
+      });
+    }
+  }, []);
+
+  const fetchPosts = useCallback(async () => {
+    const indexer = new Indexer(predefinedSporeConfigs.Aggron4.ckbIndexerUrl);
+    const { script } = predefinedSporeConfigs.Aggron4.scripts.Spore;
+    const collector = indexer.collector({
+      type: { ...script, args: '0x' },
+      lock,
+    });
+
+    const posts = [];
+    for await (const cell of collector.collect()) {
+      const unpacked = SporeData.unpack(cell.data);
+      const contentType = hex2String(unpacked.contentType.slice(2));
+
+      if (contentType !== 'application/json' || unpacked.clusterId !== id) {
+        continue;
+      }
+
+      const { title, content } =
+        JSON.parse(hex2String(unpacked.content.slice(2))) ?? {};
+      if (title && content) {
+        posts.push({
+          id: cell.cellOutput.type!.args,
+          title,
+          content,
+          outPoint: cell.outPoint!,
+        });
+      }
+    }
+    setPosts(posts);
+  }, [lock]);
 
   useEffect(() => {
-    if (!lock) {
+    if (!id) {
       return;
     }
 
-    (async () => {
-      const indexer = new Indexer(predefinedSporeConfigs.Aggron4.ckbIndexerUrl);
-      const { script } = predefinedSporeConfigs.Aggron4.scripts.Cluster;
-      const collector = indexer.collector({
-        type: { ...script, args: '0x' },
-        lock,
-      });
+    fetchSiteInfo();
+    fetchPosts();
+  }, [fetchSiteInfo, fetchPosts]);
 
-      const sites = [];
-      for await (const cell of collector.collect()) {
-        const unpacked = ClusterData.unpack(cell.data);
-        sites.push({
-          id: cell.cellOutput.type!.args,
-          name: hex2String(unpacked.name.slice(2)),
-          description: hex2String(unpacked.description.slice(2)),
-        });
-      }
-      setSites(sites);
-    })();
-  }, [lock]);
-
-  const handleCreateSite = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (!address || !lock) return;
-
-    const { txSkeleton } = await createCluster({
-      data: {
-        name: siteName,
-        description: siteDescription,
-      },
-      fromInfos: [address],
-      toLock: lock,
-    });
-    const tx = await signTransaction(txSkeleton);
-    const rpc = new RPC(predefinedSporeConfigs.Aggron4.ckbNodeUrl);
-    const hash = await rpc.sendTransaction(tx, 'passthrough');
-    console.log(hash);
-  };
-
-  if (!isConnected) {
-    return <button onClick={() => connect()}>Connect Wallet</button>;
+  if (!siteInfo) {
+    return null;
   }
 
   return (
     <div>
+      <h1>{siteInfo.name}</h1>
+      <p>{siteInfo.description}</p>
       <div>
-        <div>CKB Address: {address}</div>
-        <div>Balance: {balance} CKB</div>
-        <button onClick={() => disconnect()}>Disconnect</button>
-      </div>
-      <div>
-        <h2>Create Site</h2>
-        <form onSubmit={handleCreateSite}>
-          <div>
-            <label htmlFor="site-name">Name: </label>
-            <input
-              type="text"
-              id="name"
-              value={siteName}
-              onChange={(e) => setSiteName(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="site-name">Description: </label>
-            <input
-              type="text"
-              id="description"
-              value={siteDescription}
-              onChange={(e) => setSiteDescription(e.target.value)}
-            />
-          </div>
-          <button type="submit">Create</button>
-        </form>
-      </div>
-      <div>
-        <h2>My Sites</h2>
+        <h2>Posts</h2>
         <ul>
-          {sites.map((site) => (
-            <li key={site.id}>
-              <Link href={`/site/${site.id}`}>{site.name}</Link>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link href={`/post/${post.id}`}>{post.title}</Link>
             </li>
           ))}
         </ul>
