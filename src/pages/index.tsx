d5f098111c63e1b5c2329ec3c838a7664f653f8b
ahonn <ahonn95@outlook.com>
2023-09-12 10:29:59 +0800

    feat: add getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,74 +10,81 @@ import { useEffect, useState } from 'react';
 import { useCallback } from 'react';
 import { Post, Site } from '../type';
 import Link from 'next/link';
+import { GetServerSidePropsContext } from 'next';
 
 const id = process.env.NEXT_PUBLIC_SITE_ID;
 
-export default function SitePage() {
-  const { lock } = useWallet();
-  const [siteInfo, setSiteInfo] = useState<Site>();
-  const [posts, setPosts] = useState<Post[]>([]);
+async function fetchSiteInfo() {
+  const indexer = new Indexer(predefinedSporeConfigs.Aggron4.ckbIndexerUrl);
+  const { script } = predefinedSporeConfigs.Aggron4.scripts.Cluster;
+  const collector = indexer.collector({
+    type: { ...script, args: id as string },
+  });
 
-  const fetchSiteInfo = useCallback(async () => {
-    const indexer = new Indexer(predefinedSporeConfigs.Aggron4.ckbIndexerUrl);
-    const { script } = predefinedSporeConfigs.Aggron4.scripts.Cluster;
-    const collector = indexer.collector({
-      type: { ...script, args: id as string },
-    });
+  for await (const cell of collector.collect()) {
+    const unpacked = ClusterData.unpack(cell.data);
+    return {
+      id: cell.cellOutput.type!.args,
+      name: hex2String(unpacked.name.slice(2)),
+      description: hex2String(unpacked.description.slice(2)),
+    };
+  }
+}
 
-    for await (const cell of collector.collect()) {
-      const unpacked = ClusterData.unpack(cell.data);
-      setSiteInfo({
+async function fetchPosts() {
+  const indexer = new Indexer(predefinedSporeConfigs.Aggron4.ckbIndexerUrl);
+  const { script } = predefinedSporeConfigs.Aggron4.scripts.Spore;
+  const collector = indexer.collector({
+    type: { ...script, args: '0x' },
+  });
+
+  const posts = [];
+  for await (const cell of collector.collect()) {
+    const unpacked = SporeData.unpack(cell.data);
+    const contentType = hex2String(unpacked.contentType.slice(2));
+
+    if (contentType !== 'application/json' || unpacked.clusterId !== id) {
+      continue;
+    }
+
+    const { title, content } =
+      JSON.parse(hex2String(unpacked.content.slice(2))) ?? {};
+    if (title && content) {
+      posts.push({
         id: cell.cellOutput.type!.args,
-        name: hex2String(unpacked.name.slice(2)),
-        description: hex2String(unpacked.description.slice(2)),
+        title,
+        content,
+        outPoint: cell.outPoint!,
       });
     }
-  }, []);
-
-  const fetchPosts = useCallback(async () => {
-    const indexer = new Indexer(predefinedSporeConfigs.Aggron4.ckbIndexerUrl);
-    const { script } = predefinedSporeConfigs.Aggron4.scripts.Spore;
-    const collector = indexer.collector({
-      type: { ...script, args: '0x' },
-      lock,
-    });
-
-    const posts = [];
-    for await (const cell of collector.collect()) {
-      const unpacked = SporeData.unpack(cell.data);
-      const contentType = hex2String(unpacked.contentType.slice(2));
-
-      if (contentType !== 'application/json' || unpacked.clusterId !== id) {
-        continue;
-      }
-
-      const { title, content } =
-        JSON.parse(hex2String(unpacked.content.slice(2))) ?? {};
-      if (title && content) {
-        posts.push({
-          id: cell.cellOutput.type!.args,
-          title,
-          content,
-          outPoint: cell.outPoint!,
-        });
-      }
-    }
-    setPosts(posts);
-  }, [lock]);
-
-  useEffect(() => {
-    if (!id) {
-      return;
-    }
-
-    fetchSiteInfo();
-    fetchPosts();
-  }, [fetchSiteInfo, fetchPosts]);
-
-  if (!siteInfo) {
-    return null;
   }
+  return posts;
+}
+
+export async function getServerSideProps({ res }: GetServerSidePropsContext) {
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=59',
+  );
+
+  const siteInfo = await fetchSiteInfo();
+  const posts = await fetchPosts();
+
+  return {
+    props: {
+      siteInfo,
+      posts,
+    },
+  };
+}
+
+type SitePageProps = {
+  siteInfo: Site;
+  posts: Post[];
+};
+
+export default function SitePage(props: SitePageProps) {
+  const { siteInfo, posts } = props;
 
   return (
     <div>
