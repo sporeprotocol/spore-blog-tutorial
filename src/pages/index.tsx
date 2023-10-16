f514f4bcbf9c8517613f77d0371216d6f125309e
ahonn <ahonn95@outlook.com>
2023-10-16 15:23:02 +0800

    feat: generate ckb address

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,34 @@
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
 import { InjectedConnector } from 'wagmi/connectors/injected';
+import { commons, config, helpers } from '@ckb-lumos/lumos';
+import { useMemo } from 'react';
 
 export default function Home() {
-  const { address, isConnected } = useAccount();
+  const { address: ethAddress, isConnected } = useAccount();
   const { connect } = useConnect({
     connector: new InjectedConnector(),
   });
   const { disconnect } = useDisconnect();
 
+  const address = useMemo(() => {
+    if (!ethAddress) return;
+
+    const lock = commons.omnilock.createOmnilockScript({
+      auth: { flag: 'ETHEREUM', content: ethAddress ?? '0x' },
+    });
+    return helpers.encodeToAddress(lock, { config: config.predefined.AGGRON4 });
+  }, [ethAddress]);
+
   return (
-    <div>
-      <div>{address}</div>
-      {isConnected ? (
-        <button onClick={() => disconnect()}>Disconnect</button>
-      ) : (
-        <button onClick={() => connect()}>Connect Wallet</button>
-      )}
-    </div>
+	  <div>
+	    {isConnected ? (
+	      <div>
+	        <div>CKB Address: {address}</div>
+	        <button onClick={() => disconnect()}>Disconnect</button>
+	      </div>
+	    ) : (
+	      <button onClick={() => connect()}>Connect Wallet</button>
+	    )}
+	  </div>
   );
 }
