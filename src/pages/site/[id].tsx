89b715ff8dd13324e3687515463cd7a855995473
ahonn <ahonn95@outlook.com>
2023-10-16 16:45:01 +0800

    feat: show all blog posts

--- src/pages/site/[id].tsx
+++ src/pages/site/[id].tsx
@@ -1,16 +1,24 @@
 import useWallet from '@/hooks/useWallet';
 import { Indexer } from '@ckb-lumos/lumos';
-import { getSporeScript, unpackToRawClusterData } from '@spore-sdk/core';
+import { getSporeScript, unpackToRawClusterData, unpackToRawSporeData, bufferToRawString } from '@spore-sdk/core';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { Site } from '..';
 import { config } from '@/config';
+import Link from 'next/link';
+
+type Post = {
+  id: string;
+  title: string;
+  content: string;
+};
 
 export default function SitePage() {
   const router = useRouter();
   const { id } = router.query;
   const { lock, isConnected, connect } = useWallet();
   const [siteInfo, setSiteInfo] = useState<Site>();
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     if (!id) {
@@ -33,6 +41,36 @@ export default function SitePage() {
         });
       }
     })();
+
+    (async () => {
+      const indexer = new Indexer(config.ckbIndexerUrl);
+      const { script } = getSporeScript(config, 'Spore');
+      const collector = indexer.collector({
+        type: { ...script, args: '0x' },
+        lock,
+      });
+
+      const posts = [];
+      for await (const cell of collector.collect()) {
+        const unpacked = unpackToRawSporeData(cell.data);
+        const { contentType } = unpacked;
+
+        if (contentType !== 'application/json' || unpacked.clusterId !== id) {
+          continue;
+        }
+
+        const { title, content } =
+          JSON.parse(bufferToRawString(unpacked.content)) ?? {};
+        if (title && content) {
+          posts.push({
+            id: cell.cellOutput.type!.args,
+            title,
+            content,
+          });
+        }
+      }
+      setPosts(posts);
+    })();
   }, [id, lock]);
 
   return (
@@ -46,7 +84,16 @@ export default function SitePage() {
       ) : (
         <button onClick={() => connect()}>Connect Wallet</button>
       )}
-      <div></div>
+      <div>
+        <h2>Posts</h2>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link href={`/post/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
